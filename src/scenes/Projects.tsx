7df4185e7c52ae8ba2c437b5c0f6d1aeb60240d6
jeremynoh <jeremynohile@gmail.com>
2021-03-01 17:19:41 +0100

    🤯 - update react-use-gesture version + litle scroll fix on ios older version

--- src/scenes/Projects.tsx
+++ src/scenes/Projects.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useMemo, useState } from "react";
 import styles from "./Projects.module.css";
 import { useSprings, animated, interpolate } from "react-spring";
-import { useDrag } from "react-use-gesture";
+import { useGesture } from "react-use-gesture";
 import projectsImage, { projectsImageType } from "../assets/images/projects";
 import ContentIsVisible from "../hooks/useElementIsVisible";
 import projectsJson from '../utils/projects.json';
@@ -47,60 +47,45 @@ function Projects() {
     ...to(i),
     from: from(i),
   })); // Create a bunch of springs using the helpers above
-  // Create a gesture, we're interested in down-state, delta (current-pos - click-pos), direction and velocity
-  const bind = useDrag(
-    ({
-      args: [index],
-      down,
-      movement: [mx],
-      distance,
-      direction: [xDir],
-      velocity,
-    }) => {
-      const trigger = velocity > 0.2; // If you flick hard enough it should trigger the card to fly out
-      const dir = xDir < 0 ? -1 : 1; // Direction should either point left or right
-      if (!down && trigger) gone.add(index); // If button/finger's up and trigger velocity is reached, we flag the card ready to fly out
-      // @ts-ignore
-      set((i: any) => {
-        if (index !== i) return; // We're only interested in changing spring-data for the current spring
-        const isGone = gone.has(index);
-        const x = isGone ? (200 + window.innerWidth) * dir : down ? mx : 0; // When a card is gone it flys out left or right, otherwise goes back to zero
-        const rot = mx / 100 + (isGone ? dir * 10 * velocity : 0); // How much the card tilts, flicking it harder makes it rotate faster
-        const scale = down ? 1.1 : 1; // Active cards lift up a bit
 
-        if (isGone) {
-          if (index === currentCard) {
-            // GOOGLE ANALYTICS
-            ReactGA.event({ category: 'swipe', action: 'swipe Card' })
+  const bind = useGesture(({ args: [index], down, delta: [xDelta], distance, direction: [xDir], velocity }) => {
+    const trigger = velocity > 0.2 // If you flick hard enough it should trigger the card to fly out
+    const dir = xDir < 0 ? -1 : 1 // Direction should either point left or right
+    if (!down && trigger) gone.add(index) // If button/finger's up and trigger velocity is reached, we flag the card ready to fly out
+    // @ts-ignore
+    set((i: any) => {
+      if (index !== i) return // We're only interested in changing spring-data for the current spring
+      const isGone = gone.has(index)
+      const x = isGone ? (200 + window.innerWidth) * dir : down ? xDelta : 0 // When a card is gone it flys out left or right, otherwise goes back to zero
+      const rot = xDelta / 100 + (isGone ? dir * 10 * velocity : 0) // How much the card tilts, flicking it harder makes it rotate faster
+      const scale = down ? 1.1 : 1 // Active cards lift up a bit
 
-            if (index === 0) {
-              setCurrentCard(cards.length - 1)
-              setActiveAnimation(0);
-            } else {
-              setCurrentCard(index - 1)
-              setActiveAnimation(!activeAnimation);
-            }
+      if (isGone) {
+        if (index === currentCard) {
+          // GOOGLE ANALYTICS
+          ReactGA.event({ category: 'swipe', action: 'swipe Card' })
+
+          if (index === 0) {
+            setCurrentCard(cards.length - 1)
+            setActiveAnimation(0);
           } else {
-            // if dont swipe first card, we put the swipe card on package 
-            // @ts-ignore
-            setTimeout(() => void gone.delete(index) || set((i: any) => i === index && to(i)), 600);
+            setCurrentCard(index - 1)
+            setActiveAnimation(!activeAnimation);
           }
+        } else {
+          // if dont swipe first card, we put the swipe card on package 
+          // @ts-ignore
+          setTimeout(() => void gone.delete(index) || set((i: any) => i === index && to(i)), 600);
         }
-
-        return {
-          x,
-          rot,
-          scale,
-          delay: undefined,
-          config: { friction: 50, tension: down ? 800 : isGone ? 200 : 500 },
-        };
-      });
-      if (!down && gone.size === cards.length) {
-        // @ts-ignore
-        setTimeout(() => void gone.clear() || set((i: any) => to(i)), 600);
       }
-    }
-  );
+
+      return { x, rot, scale, delay: undefined, config: { friction: 50, tension: down ? 800 : isGone ? 200 : 500 } }
+    })
+    // @ts-ignore
+    if (!down && gone.size === cards.length) setTimeout(() => gone.clear() || set(i => to(i)), 600)
+  })
+
+
 
   const styleNameForAnimation = useMemo(() => {
     if (activeAnimation) {
